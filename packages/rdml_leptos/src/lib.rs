0118514863d43dc53b1f522d91432da4aaca7b65
Zachary Mayhew <mayhew.zachary2003@gmail.com>
2025-12-29 10:59:10 -0500

    Add spans

--- packages/rdml_leptos/src/lib.rs
+++ packages/rdml_leptos/src/lib.rs
@@ -1,17 +1,19 @@
-use proc_macro2::TokenStream;
-use quote::{ToTokens, TokenStreamExt, quote};
+use proc_macro2::{Span, TokenStream};
+use quote::{ToTokens, TokenStreamExt, quote, quote_spanned};
 use rdml::{
     Attribute, AttributeName, Block, Element, ElseNode, ExprNode, ForNode, IfNode, MatchNode,
     MatchNodeArm, Node, NodeType, Nodes,
 };
-use syn::{Expr, Stmt, parse_macro_input};
+use syn::{Expr, Stmt, parse_macro_input, spanned::Spanned, token::Paren};
 
 fn generate_attribute_name(attr_name: &AttributeName) -> TokenStream {
     match attr_name {
         AttributeName::Single(path) => quote! { #path }, // TODO: assert not path
         AttributeName::Directive(rdml::AttributeNameDirective {
-            directive, path, ..
-        }) => quote! { #directive:#path },
+            directive,
+            path,
+            colon_token,
+        }) => quote! {#directive #colon_token #path },
         AttributeName::Quoted(_lit_str) => todo!("Implement quoted attributes"),
     }
 }
@@ -21,7 +23,7 @@ fn generate_attribute(attr: &Attribute) -> TokenStream {
     let eq_token = &attr.eq_token;
     let value = &attr.value;
 
-    quote! {
+    quote_spanned! {eq_token.span()=>
         #name #eq_token {#value}
     }
 }
@@ -36,7 +38,7 @@ fn generate_element(el: &Element) -> TokenStream {
 
     let children = generate_block(&el.children);
 
-    quote! {
+    quote_spanned! {path.span()=>
         <#path #attributes>
             #children
         </#path>
@@ -55,16 +57,17 @@ fn generate_if_node(if_node: &IfNode, attrs: &[syn::Attribute]) -> TokenStream {
         .any(|attr| attr.path().get_ident().is_some_and(|id| id == "show"))
     {
         fn generate_rec(node: &IfNode) -> TokenStream {
+            let if_token = &node.if_token;
             let cond = &node.cond;
-            let fallback = node.else_branch.as_ref().map(|(_, else_branch)| {
+            let fallback = node.else_branch.as_ref().map(|(else_token, else_branch)| {
                 let else_branch = match else_branch {
                     ElseNode::If(if_node) => generate_rec(if_node),
                     ElseNode::Else(block) => generate_block(block),
                 };
-                quote! {fallback=(move || view! { #else_branch })}
+                quote_spanned! {else_token.span()=> fallback=(move || view! { #else_branch })}
             });
             let then_branch = generate_block(&node.then_branch);
-            quote! {
+            quote_spanned! {if_token.span()=>
                 <Show
                     when=(move || #cond)
                     #fallback
@@ -79,7 +82,7 @@ fn generate_if_node(if_node: &IfNode, attrs: &[syn::Attribute]) -> TokenStream {
         let if_token = &if_node.if_token;
         let cond = &if_node.cond;
         let then_branch = generate_block(&if_node.then_branch);
-        let mut result = quote! {
+        let mut result = quote_spanned! {if_token.span()=>
             #if_token #cond {
                 view! { #then_branch }.into_any()
             }
@@ -94,7 +97,7 @@ fn generate_if_node(if_node: &IfNode, attrs: &[syn::Attribute]) -> TokenStream {
                     let if_token = &if_node.if_token;
                     let cond = &if_node.cond;
                     let then_branch = generate_block(&if_node.then_branch);
-                    result.append_all(quote! {
+                    result.append_all(quote_spanned! {else_token.span()=>
                         #else_token #if_token #cond {
                             view! { #then_branch }.into_any()
                         }
@@ -103,7 +106,7 @@ fn generate_if_node(if_node: &IfNode, attrs: &[syn::Attribute]) -> TokenStream {
                 }
                 ElseNode::Else(block) => {
                     let block = generate_block(&block);
-                    result.append_all(quote! {
+                    result.append_all(quote_spanned! {else_token.span()=>
                         #else_token {
                             view! { #block }.into_any()
                         }
@@ -115,14 +118,14 @@ fn generate_if_node(if_node: &IfNode, attrs: &[syn::Attribute]) -> TokenStream {
         }
 
         if !had_else {
-            result.append_all(quote! {
+            result.append_all(quote_spanned! {if_token.span()=>
                 else {
                     view! { }.into_any()
                 }
             });
         }
 
-        quote! { {move || #result} }
+        quote_spanned! {if_token.span()=> {move || #result} }
     }
 }
 
@@ -131,25 +134,22 @@ fn generate_for_node(for_node: &ForNode, attrs: &[syn::Attribute]) -> TokenStrea
         .iter()
         .find(|attr| attr.path().get_ident().is_some_and(|id| id == "key"));
 
+    let for_token = &for_node.for_token;
     let pat = &for_node.pat;
     let expr = &for_node.expr;
     let body = generate_block(&for_node.body);
 
     if let Some(key_attr) = key_attr {
         let key: Expr = key_attr.parse_args().unwrap();
-        let a = quote! {
+        quote_spanned! {for_token.span()=>
             <For
                 each=(move || { #expr })
                 key=(move |#pat| { #key })
                 children=(move |#pat| { view! { #body } })
             />
-        };
-
-        println!("{a}");
-
-        a
+        }
     } else {
-        quote! {
+        quote_spanned! {for_token.span()=>
             {(#expr).into_iter().map(|#pat| view! { #body }).collect::<Vec<_>>()}
         }
     }
@@ -163,7 +163,7 @@ fn generate_match_node_arm(arm: &MatchNodeArm) -> TokenStream {
         .map(|(if_token, expr)| quote! { #if_token #expr });
     let fat_arrow_token = &arm.fat_arrow_token;
     let body = generate_node(&arm.body);
-    quote! {
+    quote_spanned! {fat_arrow_token.span()=>
         #pat #guard #fat_arrow_token view! { #body }.into_any(),
     }
 }
@@ -172,18 +172,26 @@ fn generate_match_node(node: &MatchNode) -> TokenStream {
     let match_token = &node.match_token;
     let expr = &node.expr;
     let arms = node.arms.iter().map(generate_match_node_arm);
-    quote! {
+    quote_spanned! {match_token.span()=>
         {move || #match_token #expr {
             #(#arms)*
         }}
     }
 }
 
+fn paren_span(paren: &Paren) -> Span {
+    let mut tokens = quote! {};
+    paren.surround(&mut tokens, |_| {});
+    tokens.span()
+}
+
 fn generate_node(node: &Node) -> TokenStream {
     let node_tokens = match &node.node {
         NodeType::Element(element) => generate_element(element),
         NodeType::Text(lit_str) => lit_str.to_token_stream(),
-        NodeType::Expr(ExprNode { expr, .. }) => quote! {{ #expr }},
+        NodeType::Expr(ExprNode { expr, paren_token }) => {
+            quote_spanned! {paren_span(&paren_token)=>{ #expr }}
+        }
         NodeType::If(if_node) => generate_if_node(if_node, &node.attrs),
         NodeType::For(for_node) => generate_for_node(for_node, &node.attrs),
         NodeType::Match(match_node) => generate_match_node(match_node),
@@ -199,7 +207,16 @@ fn generate_node(node: &Node) -> TokenStream {
         .unwrap();
 
     if !with_attr.is_empty() {
-        quote! {
+        let span = node
+            .attrs
+            .iter()
+            .filter(|attr| attr.path().get_ident().is_some_and(|id| id == "with"))
+            .next()
+            .unwrap()
+            .path()
+            .span();
+
+        quote_spanned! {span=>
             {{
                 #(#with_attr)*
                 view! { #node_tokens }
@@ -218,6 +235,8 @@ pub fn rdml(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     quote! {
         #[allow(unused_variables)]
+        #[allow(unused_parens)]
+        #[allow(unused_braces)]
         {
             ::leptos::prelude::view! {
                 #(#nodes)*
